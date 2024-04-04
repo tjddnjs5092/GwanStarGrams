8be36645bd02f9f50bfe511e8652a418f3ceb3c2
tjddnjs5092 <tjddnjs5092@naver.com>
2024-04-04 13:10:04 +0900

    1. 불필요한 콘솔 제거

--- src/navigator/AppNavigator.tsx
+++ src/navigator/AppNavigator.tsx
@@ -7,6 +7,7 @@ import {userInfoState} from '@/store/usrInfoState';
 import {MyPageScreen} from '@/screens/myPage/MyPageScreen';
 import {MypageTabViewScreen} from '@/screens/myPage/MypageTabViewScreen';
 import { BoardDetailScreen } from "@/screens/board/BoardDetailScreen";
+import DataItem from "@/types/DataItem";
 
 function AppNavigator() {
   const Stack = createNativeStackNavigator();
@@ -27,6 +28,7 @@ function AppNavigator() {
             <Stack.Screen name="MyPage" component={MyPageScreen} />
             <Stack.Screen name="MypageTabView" component={MypageTabViewScreen} />
             <Stack.Screen name="BoardDetail" component={BoardDetailScreen} />
+            <Stack.Screen name="DataItem" component={DataItem} />
           </>
         )}
       </Stack.Group>
